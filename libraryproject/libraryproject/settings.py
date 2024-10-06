ff93d62bf45606b7753a085ee3f998a04b7003a0
Ammar-Zain <alzainamar@gmail.com>
2024-10-06 17:15:20 +0300

    added lab4 (pages and styles)

--- libraryproject/libraryproject/settings.py
+++ libraryproject/libraryproject/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [( os.path.join(BASE_DIR, "apps/static"))]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
